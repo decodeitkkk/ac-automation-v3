a63eb538e0d84672bf7e94837ac42ddb54c763f7
decodeitkkk <kkailash0086@gmail.com>
2024-06-05 18:19:00 +0530

    first commit

---
+++ src/components/card.js
@@ -0,0 +1,45 @@
+let card = (img, title, desc, btn = true, w = "w-80") => `
+
+<div class=" ${w ? w : "w-80"}  p-2 mx-auto my-3 bg-white shadow-lg rounded-xl">
+    <img
+        src=${img}
+        alt=${img}
+        class="w-full p-4 m-auto h-36 object-contain"
+    />
+    <div class="p-4 m-3 bg-sky-200 rounded-lg">
+        <p class="text-xl font-bold text-slate-900 uppercase "> ${title} </p>
+        <p class="text-xs text-gray-900">
+            ${desc}
+        </p>
+    
+    
+        ${
+            btn
+                ? `
+        <div class="flex items-center justify-end">
+            <button
+                type="button"
+                class="w-10 h-10 text-base font-medium text-white bg-sky-500 rounded-full hover:bg-sky-700"
+            >
+                →
+            </button>
+        </div>
+    `
+                : ""
+        }
+        
+    </div>
+</div>
+`;
+
+let card2 = (Img) => `
+${Img.map(
+    (img) =>
+        `<div class="   mx-2  " >
+    <img
+        src="${img}"
+        alt="Slide 1"
+        class="  object-contain     "
+    />
+    </div>`
+).join("")} `;
