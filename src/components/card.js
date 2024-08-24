579f014e4e83c226e72f8d002a92ac759c005cd5
decodeitkkk <kkailash0086@gmail.com>
2024-08-24 20:54:37 +0530

    breadcrumb added

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,7 @@
-let card = (img, title, desc, btn = true, w = "w-80") => `
+let card = (img, title, desc, btn = true, w = "w-80", link) => `
 
-<div class=" ${w ? w : "w-80"}  p-2 mx-auto my-3 bg-white shadow-lg rounded-xl">
-    <img
-        src=${img}
-        alt=${img}
-        class="w-full p-4 m-auto h-36 object-contain"
-    />
+<div class=" ${w ? w : " w-80"} p-2 mx-auto my-3 bg-white shadow-lg rounded-xl">
+    <img src=${img} alt=${img} class="w-full p-4 m-auto h-36 object-contain" />
     <div class="p-4 m-3 bg-sky-200 rounded-lg">
         <p class="text-xl font-bold text-slate-900 uppercase "> ${title} </p>
         <p class="text-xs text-gray-900">
@@ -17,10 +13,12 @@ let card = (img, title, desc, btn = true, w = "w-80") => `
         btn
         ? `
         <div class="flex items-center justify-end">
-            <button type="button"
-                class="w-10 h-10 text-base font-medium text-white bg-sky-500 rounded-full hover:bg-sky-700">
-                →
-            </button>
+            <a href="${link}"> 
+                <button type="button"
+                    class="w-10 h-10 text-base font-medium text-white bg-sky-500 rounded-full hover:bg-sky-700">
+                    →
+                </button>
+            </a>
         </div>
         `
         : ""
