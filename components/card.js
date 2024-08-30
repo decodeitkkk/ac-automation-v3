2492f1afd01459c05a906887f785ebbd2b129b4b
decodeitkkk <kkailash0086@gmail.com>
2024-08-30 19:44:42 +0530

    images added

--- components/card.js
+++ components/card.js
@@ -1,11 +1,14 @@
-let card = (img, title, desc, btn = true, w = "w-80", link) => `
+let card = (img, title, img1, img2, img3, btn = true, w = "w-80", link) => `
 
 <div class=" ${w ? w : " w-80"} p-2 mx-auto my-3 bg-white shadow-lg rounded-xl">
     <img src=${img} alt=${img} class="w-full p-4 m-auto h-36 object-contain" />
     <div class="p-4 m-3 bg-sky-200 rounded-lg">
         <p class="text-xl font-bold text-slate-900 uppercase "> ${title} </p>
-        <p class="text-xs text-gray-900">
-            ${desc}
+        <p class="text-xs text-gray-900 flex justify-center gap-x-4 ">
+
+            <img src="${img1}" class="w-16 h-16 object-contain" alt="">
+            <img src="${img2}" class="w-16 h-16 object-contain" alt="">
+            <img src="${img3}" class="w-16 h-16 object-contain" alt="">
         </p>
 
 
@@ -13,7 +16,7 @@ let card = (img, title, desc, btn = true, w = "w-80", link) => `
         btn
         ? `
         <div class="flex items-center justify-end">
-            <a href="${link}"> 
+            <a href="${link}">
                 <button type="button"
                     class="w-10 h-10 text-base font-medium text-white bg-sky-500 rounded-full hover:bg-sky-700">
                     →
