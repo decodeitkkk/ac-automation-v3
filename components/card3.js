15a795ae44f258d1a12f5a5a40366d51cf673381
decodeitkkk <kkailash0086@gmail.com>
2025-08-29 17:58:40 +0530

    up till suction cups

--- components/card3.js
+++ components/card3.js
@@ -1,9 +1,9 @@
 let card = (img, title, btn = true, w = "w-80", link) => `
 
 <div class=" ${w ? w : " w-44"} p-2 mx-auto my-3 bg-white shadow-lg rounded-xl">
-        <a href=${link} class="text-center font-normal text-lg hover:text-blue-600">
+        <a href=${link} target="_blank" class="text-center font-normal text-lg hover:text-blue-600">
                 <img src=${img} alt=${img} class="w-full p-4 m-auto h-36 object-contain" />
-                <div class="text-center font-sans text-sm my-2">
+                <div class="text-center font-sans text-sm my-2  h-full      ">
                 ${title}                        
                 </div>
         </a>
