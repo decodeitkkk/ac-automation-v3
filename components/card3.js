60723fbedc11a63d8f544d7ad8e5c454cdb41436
decodeitkkk <kkailash0086@gmail.com>
2025-08-29 16:03:32 +0530

    fix link

--- components/card3.js
+++ components/card3.js
@@ -1,11 +1,11 @@
-let card = (img, title,  btn = true, w = "w-80", link) => `
+let card = (img, title, btn = true, w = "w-80", link) => `
 
 <div class=" ${w ? w : " w-44"} p-2 mx-auto my-3 bg-white shadow-lg rounded-xl">
-        <a href=${link} class="text-center font-semibold text-lg hover:text-blue-600">
+        <a href=${link} class="text-center font-normal text-lg hover:text-blue-600">
                 <img src=${img} alt=${img} class="w-full p-4 m-auto h-36 object-contain" />
-        </a>
-        <div class="text-center font-sans text-sm my-2">
+                <div class="text-center font-sans text-sm my-2">
                 ${title}                        
-        </div>
+                </div>
+        </a>
 </div>
 `;
